8e718d0e52c01d939008477c48797ee270a56521
Valeria Berrocal <valeriaberrocal@MacBook-Pro-de-Valeria-2.local>
2019-02-19 11:12:47 -0500

    everything

--- app.js
+++ app.js
@@ -1,223 +1,177 @@
-// 1
 
-var questions = [
-  [
-    "¿Que se utiliza para estilizar un sitio web?",
-    "Javascript",
-    "CSS",
-    "PHP",
-    "AngularJS",
-    1
-  ],
-  [
-    "¿Qué tipo de lenguaje es PHP?",
-    "Interpretado",
-    "Compilado",
-    "Los 2 anteriores",
-    "Ninguno de los anteriores",
-    0
-  ],
-  [
-    "¿jQuery es una biblioteca para qué lenguaje?",
-    "Python",
-    "PHP",
-    "Java",
-    "Ninguno de los anteriores",
-    3
-  ],
-  [
-    "¿Cómo se marca el inicio de código PHP?",
-    "&lt;?php",
-    "&lt;?",
-    "Los 2 anteriores",
-    "Ninguno de los anteriores",
-    2
-  ],
-  [
-    "¿Quién diseño Javascript?",
-    "Mark Zuckerberg",
-    "Bill Gates",
-    "Brendan Eich",
-    "Rasmus Lerdorf",
-    2
-  ],
+const next = document.getElementById('next');
+const firstScreen = document.getElementById('login');
+const options = document.getElementById('options');
+options.style.display='none';
 
-];
-// 2
-var questionTemplate = _.template(" \
-<div class='card question'><span class='question'><%= question %></span> \
-<ul class='options'> \
-<li> \
-<input type='radio' name='question[<%= index %>]' value='0' id='q<%= index %>o1'> \
-<label for='q<%= index %>o1'><%= a %></label> \
-</li> \
-<li> \
-<input type='radio' name='question[<%= index %>]' value='1' id='q<%= index %>o2'> \
-<label for='q<%= index %>o2'><%= b %></label> \
-</li> \
-<li> \
-<input type='radio' name='question[<%= index %>]' value='2' id='q<%= index %>o3'> \
-<label for='q<%= index %>o3'><%= c %></label> \
-</li> \
-<li> \
-<input type='radio' name='question[<%= index %>]' value='3' id='q<%= index %>o4'> \
-<label for='q<%= index %>o4'><%= d %></label> \
-</li> \
-</ul> \
-</div> \
-");
+paisQuestion1.style.display = 'none';
+paisQuestion2.style.display= 'none';
+paisQuestion3.style.display= 'none';
+techQuestion1.style.display = 'none';
+techQuestion2.style.display = 'none';
+techQuestion3.style.display = 'none';
+ultimaPantalla.style.display='none';
+lastScreen.style.display='none';
+thanksScreen.style.display = 'none';
+gracias.style.display ='none';
+let contador=0;
+let contador2=0;
 
-// 3
-var points,
-  pointsPerQuestion,
-  currentQuestion,
-  questionTimer,
-  timeForQuestion = 8, // seconds
-  timeLeftForQuestion;
-// 4
-$(function() {
-  // 5
-  $('button.start').click(start);
-  $('.play_again button').click(restart);
 
-  // 6
-  function restart() {
-    points = 0;
-    pointsPerQuestion = 10;
-    currentQuestion = 0;
-    timeLeftForQuestion = timeForQuestion;
-    // 7
-    $('.finish.card').hide();
-    $('div.start').show();
-    $('.times_up').hide();
+next.addEventListener('click', function(){
+  const userName = document.getElementById('user').value;
+  document.getElementById('welcome-text').innerHTML ="Bienvenid@ " + userName;
+  firstScreen.style.display = 'none';
+  options.style.display = 'block';
+})
 
-    generateCards();
-    updateTime();
-    updatePoints();
+const paises = document.getElementById('paises');
+paises.addEventListener('click', () =>{
+  options.style.display = 'none';
+  paisQuestion1.style.display = 'block';
+})
+
+const siguiente = document.getElementById('siguiente');
+siguiente.addEventListener('click', () =>{
+  var radios = document.getElementsByName('capital1');
+  for (var i = 0; i < radios.length; i++)
+{
+ if (radios[i].checked)
+ {
+  if(radios[i].value == "dublin"){
+    contador++;
   }
+ }
+ }
+  paisQuestion1.style.display = 'none';
+  paisQuestion2.style.display= 'block';
+  
+})
 
-  // 8
-  function start() {
-    $('div.start').fadeOut(200, function() {
-      moveToNextQuestion();
-    });
+const siguiente2 = document.getElementById('siguiente2');
+siguiente2.addEventListener('click', () =>{
+  var radios = document.getElementsByName('capital2');
+  for (var i = 0; i < radios.length; i++)
+{
+ if (radios[i].checked)
+ {
+  if(radios[i].value == "paris"){
+    contador++;
   }
+ }
+ }
+  paisQuestion2.style.display = 'none';
+  paisQuestion3.style.display= 'block';
+})
 
-  // 9
-  function generateCards() {
-    $('.questions').html('');
-    for (var i = 0; i < questions.length; i++) {
-      var q = questions[i];
-      var html = questionTemplate({
-        question: q[0],
-        index: i,
-        a: q[1],
-        b: q[2],
-        c: q[3],
-        d: q[4]
-      });
-      $('.questions').append(html);
-    };
 
-    // 10
-    $('.question.card input').change(optionSelected);
+ 
+
+  
+
+const results = document.getElementById('results');
+results.addEventListener('click', () =>{
+  var radios = document.getElementsByName('capital3');
+  for (var i = 0; i < radios.length; i++)
+{
+ if (radios[i].checked)
+ {
+  if(radios[i].value == "lima"){
+    contador++;
   }
+ }
+ }
 
-  // 11
-  function moveToNextQuestion() {
-    currentQuestion += 1;
-    if (currentQuestion > 1) {
-      $('.question.card:nth-child(' + (currentQuestion - 1) + ')').hide();
-    }
+  paisQuestion3.style.display = 'none';
+  ultimaPantalla.style.display ='block';
+  document.getElementById('right-answers1').innerHTML="Tienes "+ contador + " puntos";
+})
 
-    // 12
-    showQuestionCardAtIndex(currentQuestion);
-    setupQuestionTimer();
+const playagain=document.getElementById('playagain');
+playagain.addEventListener('click', () =>{
+  contador=0;
+  
+  ra.style.display='none';
+  ultimaPantalla.style.display='none';
+  options.style.display='block';
+ 
+})
+
+const end = document.getElementById('end');
+end.addEventListener('click', () =>{
+   ra.style.display='none';
+  ultimaPantalla.style.display='none';
+  gracias.style.display = 'block';
+})
+const mujerestech = document.getElementById('mujerestech');
+mujerestech.addEventListener('click', () =>{
+  options.style.display ='none';
+  techQuestion1.style.display = 'block';
+})
+
+const siguiente3 = document.getElementById('siguiente3');
+siguiente3.addEventListener('click', () =>{
+  var radios = document.getElementsByName('mujeres1');
+  for (var i = 0; i < radios.length; i++)
+{
+ if (radios[i].checked)
+ {
+  if(radios[i].value == "ada"){
+    contador2++;
   }
+ }
+ }
+  techQuestion1.style.display = 'none';
+  techQuestion2.style.display= 'block';
+})
 
-  // 13
-  function setupQuestionTimer() {
-    if (currentQuestion > 1) {
-      clearTimeout(questionTimer);
-    }
-    timeLeftForQuestion = timeForQuestion;
-    // 14
-    questionTimer = setTimeout(countdownTick, 1000);
+const siguiente4 = document.getElementById('siguiente4');
+siguiente4.addEventListener('click', () =>{
+  var radios = document.getElementsByName('mujeres2');
+  for (var i = 0; i < radios.length; i++)
+{
+ if (radios[i].checked)
+ {
+  if(radios[i].value == "margaret"){
+    contador2++;
   }
+ }
+ }
+  techQuestion2.style.display = 'none';
+  techQuestion3.style.display= 'block';
+})
 
-  // 15
-  function showQuestionCardAtIndex(index) { // staring at 1
-    var $card = $('.question.card:nth-child(' + index + ')').show();
+const resultados2 = document.getElementById('resultados2');
+resultados2.addEventListener('click', () =>{
+  var radios = document.getElementsByName('mujeres3');
+  for (var i = 0; i < radios.length; i++)
+{
+ if (radios[i].checked)
+ {
+  if(radios[i].value == "seis"){
+    contador2++;
   }
+ }
+ }
+  techQuestion3.style.display = 'none';
+  lastScreen.style.display ='block';
+  document.getElementById('right-answers2').innerHTML = "Tienes " + contador2 + " puntos";
+  
+})
 
-  // 16
-  function countdownTick() {
-    timeLeftForQuestion -= 1;
-    updateTime();
-    if (timeLeftForQuestion == 0) {
-      return finish();
-    }
-    questionTimer = setTimeout(countdownTick, 1000);
-  }
 
-  // 17
-  function updateTime() {
-    $('.countdown .time_left').html(timeLeftForQuestion + 's');
-  }
+const playAgain=document.getElementById('playAgain');
+playAgain.addEventListener('click', () =>{
+  contador2=0;
+  ra2.style.display='none';
+  lastScreen.style.display='none';
+  options.style.display='block';
+  
+})
 
-  // 18
-  function updatePoints() {
-    $('.points span.points').html(points + ' puntos');
-  }
-
-  // 19
-  function optionSelected() {
-    var selected = parseInt(this.value);
-    var correct = questions[currentQuestion - 1][5];
-
-    if (selected == correct) {
-      points += pointsPerQuestion;
-      updatePoints();
-      correctAnimation();
-    } else {
-      wrongAnimation();
-    }
-
-    if (currentQuestion == questions.length) {
-      clearTimeout(questionTimer);
-      return finish();
-    }
-    moveToNextQuestion();
-  }
-
-  // 20
-  function correctAnimation() {
-    animatePoints('right');
-  }
-
-  // 21
-  function wrongAnimation() {
-    animatePoints('wrong');
-  }
-
-  // 22
-  function animatePoints(cls) {
-    $('header .points').addClass('animate ' + cls);
-    setTimeout(function() {
-      $('header .points').removeClass('animate ' + cls);
-    }, 500);
-  }
-
-  // 23
-  function finish() {
-    if (timeLeftForQuestion == 0) {
-      $('.times_up').show();
-    }
-    $('p.final_points').html(points + ' puntos');
-    $('.question.card:visible').hide();
-    $('.finish.card').show();
-  }
-
-  // 24
-  restart();
-});
+const salir = document.getElementById('salir');
+salir.addEventListener('click', () =>{
+   ra2.style.display='none';
+  lastScreen.style.display = 'none';
+  thanksScreen.style.display ='block';
+})
